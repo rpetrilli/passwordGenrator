455e195f69849feac119f6ebb178bb63a5938b17
Francesco Paolo Dellaquila <paolodellaquila1999@gmail.com>
2020-02-08 09:31:29 +0100

    fix iml file compiler error; build for ios

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/francescopaolodellaquila/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/francescopaolodellaquila/Desktop/passwordGenrator"
-export "FLUTTER_TARGET=/Users/francescopaolodellaquila/Desktop/passwordGenrator/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/francescopaolodellaquila/passwordGenrator"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/francescopaolodellaquila/Documents/flutter/bin/cache/artifacts/engine/ios"
-export "TRACK_WIDGET_CREATION=true"
+export "FLUTTER_FRAMEWORK_DIR=/Users/francescopaolodellaquila/Documents/flutter/bin/cache/artifacts/engine/ios-release"
